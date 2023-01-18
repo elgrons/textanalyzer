c8ac0426078721566aa3ef863d19803457051145
Vera Weikel <vera.weikel@gmail.com>
2023-01-18 09:45:51 -0800

    It will update wordCounter() to return 0 if empty to js file.
    Co-authored-by: Eliot Gronstal <eliot.lauren@gmail.com>.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 // Business Logic
 
+/* test 2
 function wordCounter(text) {
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}*/
+
+// passes 3 tests
+function wordCounter(text) {
+  if(text.length === 0){
+      return 0;
+  }  
   let wordCount = 0;
   const textArray = text.split(" ");
   textArray.forEach(function(word) {
